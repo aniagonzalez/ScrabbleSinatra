47e0764a1e12e28244c2ecd3853bf9fb55042101
Ania Gonzalez <aniagr@gmail.com>
2016-04-10 18:06:58 -0700

    Final commit

--- lib/Scoring.rb
+++ lib/Scoring.rb
@@ -1,21 +1,4 @@
 class Scoring
-  attr_reader :word1, :word1_score, :word2, :word2_score, :word3, :word3_score, :word4, :word4_score, :word5, :word5_score
-
-  def initialize(options = {})
-    @word1 = options["word1"]
-    @word2 = options["word2"]
-    @word3 = options["word3"]
-    @word4 = options["word4"]
-    @word5 = options["word5"]
-
-    @word1_score = score(options["word1"])
-    @word2_score = score(options["word2"])
-    @word3_score = score(options["word3"])
-    @word4_score = score(options["word4"])
-    @word5_score = score(options["word5"])
-
-  end
-
   LETTER_VALUE = { "A"=> 1, "E"=> 1, "I"=> 1, "O"=> 1, "U"=> 1, "L"=> 1, "N"=> 1, "R"=> 1, "S"=> 1, "T"=> 1,
                    "D"=> 2, "G"=> 2,
                    "B"=> 3, "C"=> 3, "M"=> 3, "P"=> 3,
@@ -25,11 +8,38 @@ class Scoring
                    "Q"=> 10, "Z"=> 10
                  }
 
-  def score(word)                    #this is going to find the total value of the word.... self. bc its a method of the class, not an instance (the numbers are always the same for the letters)
+  def self.score(word)                    #this is going to find the total value of the word.... self. bc its a method of the class, not an instance (the numbers are always the same for the letters)
     total = 0                             #defining total (variable) as zero for now
     word.upcase.each_char do |key|        #turning the word into upcase and iterating through each character(letter) of the word
       total = total + LETTER_VALUE[key]   #tally the total sum of the value of those letters, when we call [key] on LETTER_VALUE it will give us the value of that key and add it to our total
     end
     word.length == 7 ? total + 50 : total #Return is implicit   #if the word's length is 7 then we add 50 to our total/ this is an ternary!!!
   end
+
+  def self.letter_hashes(word)   # [{index: 0, letter: "L", value: 4}, {...}]
+    array = []
+    index_num = 0
+    word.upcase.each_char do |char|
+      hash = {}
+      hash[:index] = index_num
+      hash[:letter] = char
+      hash[:value] = LETTER_VALUE[char]
+      array << hash
+      index_num += 1
+    end
+    return array
+  end
+
+  def self.multiple_word_hashes(words) # [{word: "hello", score: 45, letter_hashes: [{index: 0, letter: "H", value: 4}, {...}] }
+    array = []
+    words.each do |word|
+      hash = {}
+      hash[:word] = word
+      hash[:score] = self.score(word)
+      hash[:letter_hashes_array] = self.letter_hashes(word)
+      array << hash
+    end
+    return array
+  end
+
 end
--- myapp.rb
+++ myapp.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 require_relative 'lib/Scoring'
 
 class MyApp < Sinatra::Base
+  # HELPERS ##
+  helpers do
+    def active_page?(path='')
+      request.path_info == '/' + path
+    end
+  end
+  ################
+  
 
   get '/' do
     erb :index
@@ -12,18 +20,24 @@ class MyApp < Sinatra::Base
   end
 
   post '/score' do
-    @scored_words = Scoring.new(params["scorewords"])
+    @scored_word = Scoring.score(params["word1"])
+    @letter_hashes = Scoring.letter_hashes(params["word1"])
     erb :score
   end
 
-  get '/score_many' do
+  get '/how-many' do
+    erb :how_many
+  end
+
+  post '/score-many' do
+    @number = params["number"].to_i
+    if params["all_words"].nil? == false
+      @scored_words = Scoring.multiple_word_hashes(params["all_words"])
+    end
     erb :score_many
   end
 
-  post '/score_many' do
-    @scored_words = Scoring.new(params["scorewords"])
-    erb :score_many
-  end
+
 
 run!
 end
